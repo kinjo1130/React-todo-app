8700202ccf8817fe810f5d7cfee1150b36448928
kinjyo shotaro <abcshotaro616@gmail.com>
2022-07-07 00:31:22 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import './App.css';
+//function App() {} これだとエラーになる.
+//main.tsxの方でエラーを吐く、おそらく名前付きimportみたいな話だろうと思う
+
+type Todo = {
+  value: string;
+  readonly id: number;
+}
+export const App = () => {
+  const [text, setText] = useState('');
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+
+  const addTodo = () => {
+    if (!text) return
+    const newTodo: Todo = {
+      value: text,
+      id: new Date().getTime()
+    }
+    setTodos([newTodo, ...todos]);
+    setText('');
+  }
+
+  return (
+    <div>
+      {/* e.preventDefaultがないとページごとリロードされてしますな。それはよろしくない */}
+      <form onSubmit={(e) => { e.preventDefault(); addTodo() }}>
+        <input type="text" value={text} onChange={handleOnChange} />
+        <input
+          type="submit"
+          value="追加"
+          onSubmit={addTodo}
+        />
+      </form>
+      <ul>{
+        todos.map((todo) => { return <li key={todo.id}>{todo.value}</li> })
+      }</ul>
+    </div >
+  )
+}
+
+export default App
